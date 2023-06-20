e073cb2e2e616ea2779edf3bc519d57665b38864
JaeyoungAhn <quaki@naver.com>
2023-06-20 14:46:08 +0900

    refactor : 단단한 결합

--- src/main/java/com/programmers/com/kdtspringorder/Order.java
+++ src/main/java/com/programmers/com/kdtspringorder/Order.java
@@ -7,25 +7,21 @@ public class Order {
     private final UUID orderid;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    private long discountAmount;
+    private FixedAmountVoucher fixedAmountVoucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    public Order(UUID orderid, UUID customerId, List<OrderItem> orderItems, long discountAmount, OrderStatus orderStatus) {
+    public Order(UUID orderid, UUID customerId, List<OrderItem> orderItems, long discountAmount) {
         this.orderid = orderid;
         this.customerId = customerId;
         this.orderItems = orderItems;
-        this.discountAmount = discountAmount;
+        this.fixedAmountVoucher = new FixedAmountVoucher(discountAmount);
         this.orderStatus = orderStatus;
     }
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream().map(v -> v.getProductPrice() * v.getQuantity())
                 .reduce(0L, Long::sum);
-        return beforeDiscount - discountAmount;
-    }
-
-    public void setDiscountAmount(long discountAmount) {
-        this.discountAmount = discountAmount;
+        return fixedAmountVoucher.discount(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
