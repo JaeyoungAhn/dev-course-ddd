d6ca7fc135159d41b8c97e588f68ce41662a34dc
JaeyoungAhn <quaki@naver.com>
2023-06-20 15:45:45 +0900

    refactor : 느슨한 결합(runtime dependency)

--- src/main/java/com/programmers/com/kdtspringorder/Order.java
+++ src/main/java/com/programmers/com/kdtspringorder/Order.java
@@ -7,21 +7,21 @@ public class Order {
     private final UUID orderid;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    private FixedAmountVoucher fixedAmountVoucher;
+    private Voucher voucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    public Order(UUID orderid, UUID customerId, List<OrderItem> orderItems, long discountAmount) {
+    public Order(UUID orderid, UUID customerId, List<OrderItem> orderItems, long discountAmount, Voucher voucher) {
         this.orderid = orderid;
         this.customerId = customerId;
         this.orderItems = orderItems;
-        this.fixedAmountVoucher = new FixedAmountVoucher(discountAmount);
+        this.voucher = voucher;
         this.orderStatus = orderStatus;
     }
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream().map(v -> v.getProductPrice() * v.getQuantity())
                 .reduce(0L, Long::sum);
-        return fixedAmountVoucher.discount(beforeDiscount);
+        return voucher.discount(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
