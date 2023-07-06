4b73b8609a27f679d7fe29ebd0ac840d265faae1
JaeyoungAhn <quaki@naver.com>
2023-07-06 14:02:46 +0900

    feat : XML, JSON 구분해서 리턴 기능 추가

--- src/main/java/com/programmers/com/kdtspringorder/customer/CustomerController.java
+++ src/main/java/com/programmers/com/kdtspringorder/customer/CustomerController.java
@@ -2,9 +2,11 @@ package com.programmers.com.kdtspringorder.customer;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
@@ -20,9 +22,14 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @GetMapping("/api/v1/customers")
+    @ResponseBody
+    public List<Customer> findCustomers() {
+        return customerService.getAllCustomers();
+    }
 
     @RequestMapping(value = "/customers", method = RequestMethod.GET)
-    public ModelAndView findCustomers() {
+    public ModelAndView viewCustomersPage() {
         List<Customer> allCustomers = customerService.getAllCustomers();
         return new ModelAndView("view/customers", Map.of("serverTime",
                 LocalDateTime.now(), "customers", allCustomers));
